a8a3241d8760d390b5b411bbe956bda7a65042f3
Patrick M. Decoster <p.decoster@vogels.com>
2019-04-24 14:45:02 +0200

    Created Repro for VSM not working on UserControls in Uno Context

---
+++ UnoUserControls/UnoUserControls.Shared/TestControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UnoUserControls.Shared
+{
+    public sealed partial class TestControl : UserControl
+    {
+        public TestControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void uncheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Unchecked", true);
+        }
+
+        private void checkButton_Click(object sender, RoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Checked", true);
+
+        }
+
+    }
+}
